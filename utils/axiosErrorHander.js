f6eca9bd582dc0138649eca3a89048c66ade1160
Sarath M S <sarath.ms@gmail.com>
2020-09-03 12:35:53 -0400

    Make properties from sentry_xhr object optional

--- utils/axiosErrorHander.js
+++ utils/axiosErrorHander.js
@@ -1,13 +1,13 @@
 export const axiosErrorHandler = (error) => {
   if (error?.response) {
     const errorMessage = `
-      ${error?.request?.['__sentry_xhr__'].url}
+      ${error?.request?.['__sentry_xhr__']?.url}
       ${error.response?.status} - ${error.response.statusText}
       ${error.response?.data?.error}
     `
     throw new Error(errorMessage)
     // throw new Error(` \n `)
   } else {
-    throw new Error(error?.request?.['__sentry_xhr__'].url)
+    throw new Error(error?.request?.['__sentry_xhr__']?.url)
   }
 }
